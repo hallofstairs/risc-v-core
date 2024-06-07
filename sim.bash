fa395cc1dd7bea6a9f37bca17b7b0461522b2cc5
Jett Hollister <jett.hollister@duke.edu>
2024-06-07 14:57:34 -0400

    Kill GTKWave after running

--- sim.bash
+++ sim.bash
@@ -1,8 +1,23 @@
+trap 'kill_gtkwave' INT
+
+# Function to kill the GTKWave process
+kill_gtkwave() {
+    gtkwave_pid=$(pgrep gtkwave)
+    if [ -n "$gtkwave_pid" ]; then
+        kill $gtkwave_pid
+        echo "GTKWave process terminated."
+    fi
+    exit 1
+}
+
 # Compile verilog code to executable with icarus
-iverilog -o build/sim.vvp cpu.sv cpu_tb.sv
+iverilog -g2012 -o build/sim.vvp cpu.sv cpu_tb.sv
 
 # Run the simulation
 vvp build/sim.vvp 
 
 # Observe the waveform
-gtkwave build/waveform.vcd
+gtkwave build/waveform.vcd &
+
+# Wait for the GTKWave process to finish
+wait
