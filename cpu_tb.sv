3e8f4a6da970418d91e260fbdf64c2079557cba2
Jett Hollister <jett.hollister@duke.edu>
2024-06-09 17:42:29 -0400

    Added test case

--- cpu_tb.sv
+++ cpu_tb.sv
@@ -1,5 +1,6 @@
 module cpu_tb;
   reg clk, reset;
+  bit test_case_1_passed;
 
   cpu main (
       .clk  (clk),
@@ -8,19 +9,59 @@ module cpu_tb;
 
   always #1 clk = ~clk;  // Set clock tick
 
-  // Sim and timing control
-  initial begin
-    clk   = 0;
-    reset = 1;  // Init reset to 1
+  task automatic reset_cpu;
+    begin
+      reset = 1;
+      #2 reset = 0;
+    end
+  endtask
 
-    #5 reset = 0;  // Deassert reset after 5 time units
-    #10 $finish;  // Finish sim after 10 time units
-  end
+  task automatic test_case_1;
+    begin
+      // Initialize the instruction memory with operations
+      main.instr_mem.mem[0] = 32'h00100093;  // addi x1, x0, 1
+      main.instr_mem.mem[1] = 32'h00200113;  // addi x2, x0, 2
+      main.instr_mem.mem[2] = 32'h002081b3;  // add x3, x1, x2
 
-  // Logging
+      #20;  // Wait for a few clock cycles
+
+      // Check the results in the register file
+      test_case_1_passed = (main.reg_file.regs[1] == 32'h00000001) &&
+      (main.reg_file.regs[2] == 32'h00000002) &&
+      (main.reg_file.regs[3] == 32'h00000003);
+
+
+      if (test_case_1_passed) begin
+        $display("Test case 1 passed");
+      end else begin
+        $error("Test case 1 failed");
+        $display("x1 -- Expected: %h, Result: %h", 32'h00000001, main.reg_file.regs[1]);
+        $display("x2 -- Expected: %h, Result: %h", 32'h00000002, main.reg_file.regs[2]);
+        $display("x3 -- Expected: %h, Result: %h", 32'h00000003, main.reg_file.regs[3]);
+      end
+    end
+  endtask
+
+  // Run all test cases
   initial begin
+    clk = 0;
+
+    // Logging
     $dumpfile("build/waveform.vcd");
     $dumpvars(0, cpu_tb);
+
+    reset_cpu();
+    test_case_1();
+
+    // Test case 2
+    // #10;
+    // reset_cpu();
+    // test_case_2();
+
+    // TODO: Add more test cases
+
+    #100;
+    $finish;
   end
 
 
